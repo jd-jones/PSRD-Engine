4a7f883f8e95d08710f490d3c0ccd0b8478ad318
Devon Jones <devon.jones@sendgrid.com>
2015-05-14 15:09:52 -0600

    formatting

--- src/api/weapon.js
+++ src/api/weapon.js
@@ -187,26 +187,26 @@ function sizeCategoryDiff(size1, size2) {
 
 /*
 - 1 1d2 1d3 1d4 1d6 1d8 2d6 3d6 4d6 6d6 8d6 12d6 ...
-... 1d8 1d10 2d6 ...
+... 1d8 1d10 2d8 ...
 ... 2d6 2d8 3d8 4d8 6d8 8d8 12d8 ...
 ... 1d10 1d12 3d6 ...
 ... 1d6 2d4 2d6 ...
 ... 2d8 2d10 4d8 ...
 ... 2d10 2d12 6d6 ...
 
-  T     S    M   L   H    G    C
-  -     1   1d2 1d3 1d4  1d6  1d8
-  1    1d2  1d3 1d4 1d6  1d8  2d6
- 1d2   1d3  1d4 1d6 1d8  2d6  3d6
- 1d3   1d4  1d6 1d8 2d6  3d6  4d6
- 1d4   1d6  1d8 2d6 3d6  4d6  6d6
- 1d6   1d8 1d10 2d8 3d8  4d8  6d8
- 1d8  1d10 1d12 3d6 4d6  6d6  8d6
- 1d4   1d6  2d4 2d6 3d6  4d6  6d6
- 1d8  1d10  2d6 3d6 4d6  6d6  8d6
-1d10   2d6  2d8 3d8 4d8  6d8  8d8
- 2d6   2d8 2d10 4d8 6d8  8d8 12d8
- 2d8  2d10 2d12 6d6 8d6 12d6 16d6
+  T    S    M   L   H    G    C
+  -    1   1d2 1d3 1d4  1d6  1d8
+  1   1d2  1d3 1d4 1d6  1d8  2d6
+ 1d2  1d3  1d4 1d6 1d8  2d6  3d6
+ 1d3  1d4  1d6 1d8 2d6  3d6  4d6
+ 1d4  1d6  1d8 2d6 3d6  4d6  6d6
+ 1d6  1d8 1d10 2d8 3d8  4d8  6d8
+ 1d8 1d10 1d12 3d6 4d6  6d6  8d6
+ 1d4  1d6  2d4 2d6 3d6  4d6  6d6
+ 1d8 1d10  2d6 3d6 4d6  6d6  8d6
+1d10  2d6  2d8 3d8 4d8  6d8  8d8
+ 2d6  2d8 2d10 4d8 6d8  8d8 12d8
+ 2d8 2d10 2d12 6d6 8d6 12d6 16d6
 */
 
 die_transitions = {
